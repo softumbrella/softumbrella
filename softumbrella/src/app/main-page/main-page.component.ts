8e3b5129dd5dc0ceea0cd92619326b1c2344d533
Steven Lee <softumbrella.company@gmail.com>
2025-05-06 22:38:08 -0500

    Created components and connected routing

---
+++ softumbrella/src/app/main-page/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-main-page',
+  imports: [HeaderComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
